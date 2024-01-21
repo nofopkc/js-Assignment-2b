a94f47348c5cd10d3b49da4cfd9a74e36db3e522
nofopkc <155906819+nofopkc@users.noreply.github.com>
2024-01-21 12:30:53 -0800

    Add files via upload

---
+++ Assignment-8d.js
@@ -0,0 +1,2 @@
+let userName = prompt("Please Enter Your Name..");
+alert("Assalam-o-Alaikum Mr./Miss..  "+userName+"\nHow are you?\nWelCome to The Webpage\nWe are really very thankful you to be here!");
